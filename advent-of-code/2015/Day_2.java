e9ebfda4247b719f1d200d60e7024a913005e985
Brandon Martinez <76891236+BrandonMartinez-jar@users.noreply.github.com>
2023-02-03 15:54:54 -0500

    Add files via upload

---
+++ advent-of-code/2015/Day_2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+import com.adventofcode.hiraganime.model.Exercise;
+
+
+
+public class Day_2 implements Exercise{
+	
+	public Scanner console = new Scanner(System.in); // <- Console input configurations
