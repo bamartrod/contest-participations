1dd23b5b998126f8110e8451d0d91035f7f445ae
Brandon Martinez <76891236+BrandonMartinez-jar@users.noreply.github.com>
2023-02-04 05:23:48 -0500

    Delete Day_2.java

--- advent-of-code/2015/Day_2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-import com.adventofcode.hiraganime.model.Exercise;
-
-
-
-public class Day_2 implements Exercise{
-	
-	public Scanner console = new Scanner(System.in); // <- Console input configurations
